1d5b22178f474d41127ee0a8a90c5a1a16307fb3
daarligtid <daarligtid@gmail.com>
2019-01-26 15:51:27 +0100

    merged back the deleted code from the testsuites

--- gcc/testsuite/gcc.target/mips/args-1.c
+++ gcc/testsuite/gcc.target/mips/args-1.c
@@ -12,7 +12,7 @@ const char *optimized_for = _MIPS_TUNE;
 /* Test complementary macro pairs: exactly one of each pair
    must be defined.  */
 
-#if defined (_R3000) == defined (_R4000)
+#if defined (_R3000) == defined (_R4000) && !defined (__sgi__)
 #error _R3000 / _R4000 mismatch
 #endif
 
@@ -26,8 +26,7 @@ const char *optimized_for = _MIPS_TUNE;
 
 /* Check for __mips64 consistency.  */
 
-#if defined (__mips64) != defined (_R4000)
-#error __mips64 / _R4000 mismatch
+#if defined (__mips64) != defined (_R4000) && !defined (__sgi__)
 #endif
 
 #if defined (__mips64) && __mips != 3 && __mips != 4 && __mips != 64

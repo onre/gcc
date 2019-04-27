5b43e627bdbf4a78157bb0958dc64487964d84ed
Martin Liska <mliska@suse.cz>
2019-04-27 08:33:29 +0200

    Add missing target options (PR middle-end/90258).
    2019-04-27  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/90258
    	* opt-suggestions.c (option_proposer::build_option_suggestions):
    	When get_valid_option_values returns empty values, add the
    	misspelling candidate.
    2019-04-27  Martin Liska  <mliska@suse.cz>
    
    	PR middle-end/90258
    	* gcc.dg/completion-5.c: New test.
    	* gcc.target/i386/spellcheck-options-5.c: New test.
    
    From-SVN: r270622

--- gcc/testsuite/gcc.target/i386/spellcheck-options-5.c
+++ gcc/testsuite/gcc.target/i386/spellcheck-options-5.c
@@ -1,5 +1,5 @@
 /* PR middle-end/90258.  */
 
-/* { dg-do compile { target *-*-linux* *-*-gnu* } } */
+/* { dg-do compile } */
 /* { dg-options "-mandroidX" } */
 /* { dg-error "unrecognized command line option '-mandroidX'; did you mean '-mandroid'"  "" { target *-*-* } 0 } */

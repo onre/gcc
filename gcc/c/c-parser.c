a183ee41bde401a288b57e312f912a9730000e09
Thomas Schwinge <thomas@codesourcery.com>
2019-05-17 21:13:15 +0200

    [PR89433] Use 'oacc_verify_routine_clauses' for C/C++ OpenACC 'routine' directives
    	gcc/
    	PR middle-end/89433
    	* omp-general.c (oacc_build_routine_dims): Move some of its
    	processing into...
    	(oacc_verify_routine_clauses): ... this new function.
    	* omp-general.h (oacc_verify_routine_clauses): New prototype.
    	gcc/c/
    	PR c/89433
    	* c-parser.c (c_parser_oacc_routine): Normalize order of clauses.
    	(c_finish_oacc_routine): Call oacc_verify_routine_clauses.
    	gcc/cp/
    	PR c++/89433
    	* parser.c (cp_parser_oacc_routine)
    	(cp_parser_late_parsing_oacc_routine): Normalize order of clauses.
    	(cp_finalize_oacc_routine): Call oacc_verify_routine_clauses.
    	gcc/testsuite/
    	PR testsuite/89433
    	* c-c++-common/goacc/routine-2.c: Update, and move some test
    	into...
    	* c-c++-common/goacc/routine-level-of-parallelism-1.c: ... this
    	new file.
    
    From-SVN: r271344

--- gcc/c/c-parser.c
+++ gcc/c/c-parser.c
@@ -15802,6 +15802,9 @@ c_parser_oacc_routine (c_parser *parser, enum pragma_context context)
       data.clauses
 	= c_parser_oacc_all_clauses (parser, OACC_ROUTINE_CLAUSE_MASK,
 				     "#pragma acc routine");
+      /* The clauses are in reverse order; fix that to make later diagnostic
+	 emission easier.  */
+      data.clauses = nreverse (data.clauses);
 
       if (TREE_CODE (decl) != FUNCTION_DECL)
 	{
@@ -15816,6 +15819,9 @@ c_parser_oacc_routine (c_parser *parser, enum pragma_context context)
       data.clauses
 	= c_parser_oacc_all_clauses (parser, OACC_ROUTINE_CLAUSE_MASK,
 				     "#pragma acc routine");
+      /* The clauses are in reverse order; fix that to make later diagnostic
+	 emission easier.  */
+      data.clauses = nreverse (data.clauses);
 
       /* Emit a helpful diagnostic if there's another pragma following this
 	 one.  Also don't allow a static assertion declaration, as in the
@@ -15878,6 +15884,8 @@ c_finish_oacc_routine (struct oacc_routine_data *data, tree fndecl,
       data->error_seen = true;
       return;
     }
+
+  oacc_verify_routine_clauses (&data->clauses, data->loc);
 
   if (oacc_get_fn_attrib (fndecl))
     {

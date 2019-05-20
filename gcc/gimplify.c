c9a7c27ae4a70429e3ac11b268fa7ae703eeee4a
Jakub Jelinek <jakub@redhat.com>
2019-05-20 23:33:46 +0200

    re PR c++/59813 (tail-call elimination didn't fire for left-shift of char to cout)
    	PR c++/59813
    	PR target/90418
    	* function.h (struct function): Add calls_eh_return member.
    	* gimplify.c (gimplify_call_expr): Set cfun->calls_eh_return when
    	gimplifying __builtin_eh_return call.
    	* tree-inline.c (initialize_cfun): Copy calls_eh_return from src_cfun
    	to cfun.
    	(expand_call_inline): Or in src_cfun->calls_eh_return into
    	dst_cfun->calls_eh_return.
    	* tree-tailcall.c (suitable_for_tail_call_opt_p): Return false if
    	cfun->calls_eh_return.
    	* lto-streamer-in.c (input_struct_function_base): Read calls_eh_return.
    	* lto-streamer-out.c (output_struct_function_base): Write
    	calls_eh_return.
    
    From-SVN: r271440

--- gcc/gimplify.c
+++ gcc/gimplify.c
@@ -3305,6 +3305,10 @@ gimplify_call_expr (tree *expr_p, gimple_seq *pre_p, bool want_value)
 	  break;
 	}
 
+      case BUILT_IN_EH_RETURN:
+	cfun->calls_eh_return = true;
+	break;
+
       default:
         ;
       }

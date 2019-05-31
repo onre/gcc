4bf27e9861e78bf0828a289c982719f70fc78af2
Daniel Hams <daniel.hams@gmail.com>
2019-05-31 13:51:11 +0100

    Allow running the fixincludes tests by adding back expected results files.

--- fixincludes/tests/base/complex.h
+++ fixincludes/tests/base/complex.h
@@ -21,6 +21,12 @@
 #endif  /* HPUX_IMAGINARY_I_CHECK */
 
 
+#if defined( IRIX_COMPLEX_CHECK )
+#define _Complex_I (__extension__ 1.0iF)
+#define I _Complex_I
+#endif  /* IRIX_COMPLEX_CHECK */
+
+
 #if defined( SOLARIS_COMPLEX_CHECK )
 #define _Complex_I (__extension__ 1.0iF)
 #define	complex		_Complex

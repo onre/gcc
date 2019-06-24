b140cccd924a59ebe3351dd4b2354649579ecfda
Richard Biener <rguenther@suse.de>
2019-06-24 17:21:40 +0000

    re PR target/90972 (ICE in fold_convert_loc, at fold-const.c:2429)
    2019-06-24  Richard Biener  <rguenther@suse.de>
    
    	PR tree-optimization/90972
    	* tree-vect-stmts.c (vect_init_vector): Handle CONSTANT_CLASS_P
    	in common code, dealing with STRING_CST properly.
    
    	* gcc.dg/torture/pr90972.c: New testcase.
    
    From-SVN: r272623

--- gcc/tree-vect-stmts.c
+++ gcc/tree-vect-stmts.c
@@ -1492,12 +1492,8 @@ vect_init_vector (stmt_vec_info stmt_info, tree val, tree type,
 		   promotion of invariant/external defs.  */
 		val = gimple_convert (&stmts, TREE_TYPE (type), val);
 	      for (gimple_stmt_iterator gsi2 = gsi_start (stmts);
-		   !gsi_end_p (gsi2); )
-		{
-		  init_stmt = gsi_stmt (gsi2);
-		  gsi_remove (&gsi2, false);
-		  vect_init_vector_1 (stmt_info, init_stmt, gsi);
-		}
+		   !gsi_end_p (gsi2); gsi_next (&gsi2))
+		vect_init_vector_1 (stmt_info, gsi_stmt (gsi2), gsi);
 	    }
 	}
       val = build_vector_from_val (type, val);

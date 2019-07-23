4f45fc34a9a3f2eff354833cb7ff283c7566d031
Ren Kimura <rkx1209dev@gmail.com>
2019-07-23 07:33:32 +0000

    libiberty: Check zero value shstrndx in simple-object-elf.c
    From-SVN: r273718

--- libiberty/simple-object-elf.c
+++ libiberty/simple-object-elf.c
@@ -548,7 +548,7 @@ simple_object_elf_match (unsigned char header[SIMPLE_OBJECT_MATCH_HEADER_LEN],
       XDELETE (eor);
       return NULL;
     }
-
+  
   if (eor->shstrndx == 0)
     {
       *errmsg = "invalid ELF shstrndx == 0";
@@ -556,7 +556,7 @@ simple_object_elf_match (unsigned char header[SIMPLE_OBJECT_MATCH_HEADER_LEN],
       XDELETE (eor);
       return NULL;
     }
-
+  
   return (void *) eor;
 }
 

cafbb5dcd73b8f86638ca81b3d2cd56274d4a172
Aaron Sawdey <acsawdey@linux.ibm.com>
2020-11-17 17:17:44 -0600

    Additional small changes to support opaque modes
    After building some larger codes using opaque types and some c++ codes
    using opaque types it became clear I needed to go through and look for
    places where opaque types and modes needed to be handled. A whole pile
    of one-liners.
    
    gcc/
    	* typeclass.h: Add opaque_type_class.
    	* builtins.c (type_to_class): Identify opaque type class.
    	* dwarf2out.c (is_base_type): Handle opaque types.
    	(gen_type_die_with_usage): Handle opaque types.
    	* expr.c (count_type_elements): Opaque types should
    	never have initializers.
    	* ipa-devirt.c (odr_types_equivalent_p): No type-specific handling
    	for opaque types is needed as it eventually checks the underlying
    	mode which is what is important.
    	* tree-streamer.c (record_common_node): Handle opaque types.
    	* tree.c (type_contains_placeholder_1): Handle opaque types.
    	(type_cache_hasher::equal): No additional comparison needed for
    	opaque types.
    gcc/c-family
    	* c-pretty-print.c (c_pretty_printer::simple_type_specifier):
    	Treat opaque types like other types.
    	(c_pretty_printer::direct_abstract_declarator): Opaque types are
    	supported types.
    gcc/c
    	* c-aux-info.c (gen_type): Support opaque types.
    gcc/cp
    	* error.c (dump_type): Handle opaque types.
    	(dump_type_prefix): Handle opaque types.
    	(dump_type_suffix): Handle opaque types.
    	(dump_expr): Handle opaque types.
    	* pt.c (tsubst): Allow opaque types in templates.
    	(unify): Allow opaque types in templates.
    	* typeck.c (structural_comptypes): Handle comparison
    	of opaque types.

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -13058,6 +13058,7 @@ is_base_type (tree type)
       return 1;
 
     case VOID_TYPE:
+    case OPAQUE_TYPE:
     case ARRAY_TYPE:
     case RECORD_TYPE:
     case UNION_TYPE:
@@ -25849,6 +25850,7 @@ gen_type_die_with_usage (tree type, dw_die_ref context_die,
       return;
 
     case VOID_TYPE:
+    case OPAQUE_TYPE:
     case INTEGER_TYPE:
     case REAL_TYPE:
     case FIXED_POINT_TYPE:

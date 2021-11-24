56a8ca0ab70e53351084a9347e16df5aee187a11
Richard Biener <rguenther@suse.de>
2021-11-24 15:57:03 +0100

    Remove unreachable returns
    This removes unreachable return statements as diagnosed by
    the -Wunreachable-code patch.  Some cases are more obviously
    an improvement than others - in fact some may get you the idea
    to replace them with gcc_unreachable () instead, leading to
    cases of the 'Remove unreachable gcc_unreachable () at the end
    of functions' patch.
    
    2021-11-25  Richard Biener  <rguenther@suse.de>
    
    	* vec.c (qsort_chk): Do not return the void return value
    	from the noreturn qsort_chk_error.
    	* ccmp.c (expand_ccmp_expr_1): Remove unreachable return.
    	* df-scan.c (df_ref_equal_p): Likewise.
    	* dwarf2out.c (is_base_type): Likewise.
    	(add_const_value_attribute): Likewise.
    	* fixed-value.c (fixed_arithmetic): Likewise.
    	* gimple-fold.c (gimple_fold_builtin_fputs): Likewise.
    	* gimple-ssa-strength-reduction.c (stmt_cost): Likewise.
    	* graphite-isl-ast-to-gimple.c
    	(gcc_expression_from_isl_expr_op): Likewise.
    	(gcc_expression_from_isl_expression): Likewise.
    	* ipa-fnsummary.c (will_be_nonconstant_expr_predicate):
    	Likewise.
    	* lto-streamer-in.c (lto_input_mode_table): Likewise.
    
    gcc/c-family/
    	* c-opts.c (c_common_post_options): Remove unreachable return.
    	* c-pragma.c (handle_pragma_target): Likewise.
    	(handle_pragma_optimize): Likewise.
    
    gcc/c/
    	* c-typeck.c (c_tree_equal): Remove unreachable return.
    	* c-parser.c (get_matching_symbol): Likewise.
    
    libgomp/
    	* oacc-plugin.c (GOMP_PLUGIN_acc_default_dim): Remove unreachable
    	return.

--- gcc/dwarf2out.c
+++ gcc/dwarf2out.c
@@ -13379,8 +13379,6 @@ is_base_type (tree type)
 	return 0;
       gcc_unreachable ();
     }
-
-  return 0;
 }
 
 /* Given a pointer to a tree node, assumed to be some kind of a ..._TYPE
@@ -20267,7 +20265,6 @@ add_const_value_attribute (dw_die_ref die, machine_mode mode, rtx rtl)
       /* No other kinds of rtx should be possible here.  */
       gcc_unreachable ();
     }
-  return false;
 }
 
 /* Determine whether the evaluation of EXPR references any variables
